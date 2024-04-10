6aba935ff55116bb3a4f10bc9aef57f7281b7b3c
Mateusz Plata <m.grudzinski.plata@gmail.com>
2024-04-10 12:34:06 +0200

    Wiersz o Krzysiu 3D

---
+++ main.py
@@ -0,0 +1,23 @@
+print("Krzysiu z bliska spoglądał na świat")
+print("W 3D widział rzeczy, których innych brak.")
+print("Zjarał się mocno, jak płomień w kominku,")
+print("A świat przed nim się rozciągał jak morski przestwór.\n")
+print("Pokemony łapał, zbierał je w sieci,")
+print("Z każdym kolejnym krokiem czuł się jak król sieci.")
+print("Znacząco gestykulował, rzucał pokeballami,")
+print("A potem z uśmiechem na twarzy o nich opowiadał nam.\n")
+print("To Krzysio taki, w 3D świat jest mu bliski,")
+print("Pokemony zbiera, jakby to był ich misji.")
+print("A my z uśmiechem patrzymy, jak się zaraża pasją,")
+print("Bo to w 3D świat Krzysia, a my w nim gościmy z radości!")
+
+
+
+
+
+
+
+
+
+
+
