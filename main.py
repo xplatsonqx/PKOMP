a296d1742c5dd43f2ab5c49b4d4b1829f747e986
Mateusz Plata <m.grudzinski.plata@gmail.com>
2024-04-10 15:18:07 +0200

    connection to mssql server established

--- main.py
+++ main.py
@@ -1,1 +1,22 @@
-print("YOYOYOYOYO")
+import pyodbc
+
+server = 'DESKTOP-2DB5AH1\\MSSQLSERVER01'
+database = 'PKO_MP'
+username = 'login'
+password = 'pass'
+
+cnxn = pyodbc.connect('DRIVER={ODBC Driver 17 for SQL Server};\
+                      SERVER=' + server + ';\
+                      DATABASE=' + database + ';\
+                      UID=' + username + ';\
+                      PWD=' + password)
+
+cursor = cnxn.cursor()
+
+query = "SELECT * FROM Users"
+cursor.execute(query)
+rows = cursor.fetchall()
+for row in rows:
+    print(row)
+
+cursor.close()
