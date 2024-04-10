cfd50a4fdfc4791d7419799b504878558ea025ee
Mateusz Plata <m.grudzinski.plata@gmail.com>
2024-04-10 17:09:27 +0200

    connection to mssql server established

--- main.py
+++ main.py
@@ -1,3 +1,5 @@
+import random
+
 import pyodbc
 
 server = 'DESKTOP-2DB5AH1\\MSSQLSERVER01'
@@ -13,10 +15,34 @@ cnxn = pyodbc.connect('DRIVER={ODBC Driver 17 for SQL Server};\
 
 cursor = cnxn.cursor()
 
+
+
+# List of names
+names = ["James", "John", "Robert", "Michael", "William", "David", "Richard", "Joseph", "Charles", "Thomas",
+    "Mary", "Patricia", "Jennifer", "Linda", "Elizabeth", "Barbara", "Susan", "Jessica", "Sarah", "Karen"]
+
+surnames = [
+    "Smith", "Johnson", "Williams", "Jones", "Brown", "Davis", "Miller", "Wilson", "Moore", "Taylor",
+    "Anderson", "Thomas", "Jackson", "White", "Harris", "Martin", "Thompson", "Garcia", "Martinez", "Robinson"
+]
+
+for i in range(1, 1000):
+    UserID = i
+    LastName = random.choice(surnames)
+    FirstName = random.choice(names)
+    query = f"INSERT INTO Users (UserID, LastName, FirstName) VALUES ({UserID}, '{LastName}', '{FirstName}')"
+    cursor.execute(query)
+    cursor.commit()
+
 query = "SELECT * FROM Users"
 cursor.execute(query)
 rows = cursor.fetchall()
 for row in rows:
     print(row)
+
+# query = "DELETE FROM Users WHERE 1=1"
+# cursor.execute(query)
+# cursor.commit()
+
 
 cursor.close()
